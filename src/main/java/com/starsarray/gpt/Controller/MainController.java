5afffeefcdd4706dc400ea75d4fc29ed3ae5c081
star_array <945467963@qq.com>
2024-04-17 16:38:25 +0800

    优化冗余代码

--- src/main/java/com/starsarray/gpt/Controller/MainController.java
+++ src/main/java/com/starsarray/gpt/Controller/MainController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 @RestController
 @RequestMapping("api/v1/chat/gpt")
 @RequiredArgsConstructor
